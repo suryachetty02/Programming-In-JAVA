20e987bb25a77906a3e30a47581feb7316b09d30
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2023-03-23 12:19:42 +0530

    Add files via upload

---
+++ Inverted.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class IFP {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
